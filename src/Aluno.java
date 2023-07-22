07b6630aa1b06165f98dc96bf55c0498392d7adb
felipe <ffe1804@gmail.com>
2023-07-22 14:16:41 -0300

    .

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,13 +1,36 @@
 public class Aluno extends Usuario {
-int nota;
-int faltas;
+    int nota;
+    int faltas;
 
-Aluno()
+    public Aluno(String nome, String curso) {
+        super(nome, curso, "aluno");
+    }
 
-public void receberNota(){
 
+    public void receberNota(int nota) {
+        if (nota <= 100) {
+            this.nota = nota;
+        } else {
+            System.out.println("nota invalida,somente sao permitidas <=100");
+        }
+
+    }
+
+    public void receberFaltas(int faltas) {
+        if (faltas <= 120) {
+            this.faltas += faltas;
+        } else {
+            System.out.println("aluno já ultrapassou o limiute  de faltas");
+        }
+    }
+
+    @Override
+    public void apresentarInformacoes() {
+        super.apresentarInformacoes();
+        System.out.println("Suas informacoes academicas sao:");
+        System.out.println("Nota " + this.nota);
+        System.out.println("Faltas " + this.faltas);
+    }
 }
-public void receberFaltas(){
 
-}
-}
+
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,26 +1,22 @@
 public class Usuario {
-        String nome;
-        String curso;
-        String turma;
-        String sala;
-        String tipoUsuario;
-        Usuario(String nome, String curso, String tipoUsuario) {
-            this.nome = nome;
-            this.curso = curso;
-            this.tipoUsuario = tipoUsuario;
-        }
 
-            public void apresentarinformacoes(){
-                System.out.println("---------"+tipoUsuario+"---------");
-                System.out.println("Nome:"+nome);
-                System.out.println("Curso:"+curso);
-                System.out.println("turma:"+turma);
-                System.out.println("sala:"+sala);
+    String nome;
+    String turma;
+    String sala;
+    String curso;
+    String tipoUsuario;
 
+    public Usuario(String nome,  String curso,  String tipoUsuario) {
+        this.nome = nome;
+        this.curso = curso;
+        this.tipoUsuario = tipoUsuario;
+    }
 
-
-        }
-
-
+    public void apresentarInformacoes(){
+        System.out.println("-----------"+tipoUsuario+"-----------");
+        System.out.println("Nome: "+nome);
+        System.out.println(("Curso: "+curso));
+        System.out.println(("Turma: "+turma));
+        System.out.println(("sala: "+sala));
     }
 }
