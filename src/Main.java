28cc9235cad6bec3c78f88dc6d8073afc2c64954
felipe <ffe1804@gmail.com>
2023-07-22 15:18:03 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Professor  professor1 = new Professor ("felipe","Fundamentos de Java");
+        professor1.criarTurma("FJ2023");
+
+        professor1.matricularAluno(new Aluno("Joao", professor1.curso));
+        professor1.matricularAluno(new Aluno("Nicolau", professor1.curso));
+        professor1.matricularAluno(new Aluno("Feliper", professor1.curso));
+        professor1.matricularAluno(new Aluno("Maria", professor1.curso));
+        professor1.matricularAluno(new Aluno("Lucas", professor1.curso));
+
+        professor1.reservarSala("Sala1");
+        for(Aluno aluno:  professor1.listaAlunos) {
+            professor1.darNotas(aluno, 50);
+            professor1.darFaltas(aluno, 2);
+        }
+            professor1.apresentarInformacoes();
+        for(Aluno aluno:  professor1.listaAlunos) {
+            aluno.apresentarInformacoes();
+        }
     }
 }
